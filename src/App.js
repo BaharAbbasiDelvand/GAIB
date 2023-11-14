056000927acab5164303ba6e5c9dc14a8a7dfb67
abbasidb <baharwix14@gmail.com>
2023-11-14 15:47:09 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> Hello and welcome to GenAI's autotesting application: </h1>
+      <h1> Hello and welcome to GenAI's Autotesting Application: </h1>
     </div>
   );
 }
